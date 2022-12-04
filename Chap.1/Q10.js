e3c57b5834db15ac9a17b6148ed087cbe82a056b
energy1-914 <dlawldnjs5@daum.net>
2022-12-04 21:33:33 +0900

    문자열의 숫자화_풀이법 추가

--- Chap.1/Q10.js
+++ Chap.1/Q10.js
@@ -12,3 +12,15 @@ function solution(s) {
   return answer;
 }
 
+//answer2
+function solution(s) {
+  let answer = 0;
+  let nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+  for (let i = 0; i <= 9; i++) {
+    let tmp = s.split(nums[i]);
+    s = tmp.join(i);
+  }
+  answer = parseInt(s);
+  return answer;
+ }  
