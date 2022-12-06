ea13c66d6286e22ffd9a05013a6f8f93b101a6b9
energy1-914 <dlawldnjs5@daum.net>
2022-12-07 00:10:58 +0900

    한번 사용한  문자  인덱스반환

---
+++ Chap.1/Q12.js
@@ -0,0 +1,10 @@
+//한번만 사용한 문자중에서 문자열에서 가장 먼저 나타난 문자의 인덱스를 반환(인덱스는 1부터 시작. 없을 경우 -1 반환)
+
+function solution (s) {
+  let array = s.split('');
+  let newArr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    
+  }
+}
