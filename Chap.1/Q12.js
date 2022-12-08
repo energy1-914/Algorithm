8d07cf0964759367f762d7cdc715b109893b4f8a
energy1-914 <dlawldnjs5@daum.net>
2022-12-08 23:56:55 +0900

    한번 사용한 문자열 인덱스 반환

--- Chap.1/Q12.js
+++ Chap.1/Q12.js
@@ -2,9 +2,17 @@
 
 function solution (s) {
   let array = s.split('');
-  let newArr = [];
-
-  for (let i = 0; i < array.length; i++) {
-    
+  let newArr = array;
+  for (let i = 0; i < array.length; i++) {  
+    for (x of array) {
+      let idx = array.indexOf(x);
+      if (x === array[i]) newArr.splice(idx, 1);
+    }
   }
+  console.log(newArr); // [ 'a', 't', 'c' ]
+  newArr[0]
 }
+
+ solution('statistics') // a 3
+// solution('aabb') // -1
+// solution('stringshowtime') // r 3
