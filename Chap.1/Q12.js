357ed44432ac5dbe2a4f4bc2cdcb2fe376c61e53
energy1-914 <dlawldnjs5@daum.net>
2022-12-12 19:38:43 +0900

    해싱 : 한번 사용한 문자의 인덱스 반환

--- Chap.1/Q12.js
+++ Chap.1/Q12.js
@@ -1,22 +1,18 @@
 //한번만 사용한 문자중에서 문자열에서 가장 먼저 나타난 문자의 인덱스를 반환(인덱스는 1부터 시작. 없을 경우 -1 반환)
 
 function solution (s) {
-  const array = s.split('');
-  let newArr = s.split('');
-  console.log(array);
+  const sH = new Map();
 
-  for (let i = 0; i < array.length; i++) {  
-    for (x of array) {
-      let idx = array.indexOf(x);
-      if (x === array[i]) newArr.splice(idx, 1);
+  for(let x of s){
+    sH.set(x, (sH.get(x) || 0) + 1)
     }
+
+  for (let x of s) {
+    if (sH.get(x) === 1) return s.indexOf(x)+1;
   }
-  console.log(array);
-  console.log(newArr);
-  console.log(newArr[0]); // [ 'a', 't', 'c' ]
-  // console.log(array.indexOf(newArr[0]))
+  return -1;
 }
 
- solution('statistics') // a 3
-// solution('aabb') // -1
-// solution('stringshowtime') // r 3
+ console.log(solution('statistics')) // 3
+console.log(solution('aabb')) // -1
+console.log(solution('stringshowtime')) // 3
