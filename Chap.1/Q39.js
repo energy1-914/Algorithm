2d8767168837bfd7ca3f69f6b880f1befe81afa1
energy1-914 <dlawldnjs5@daum.net>
2022-12-22 17:41:11 +0900

    스택, 큐 : 공주 구하기_풀이법 추가

--- Chap.1/Q39.js
+++ Chap.1/Q39.js
@@ -1,11 +1,7 @@
 // 공주 구하기. n번째 사람이 n을 외치고, n=k인 경우 제외시킨다. 마지막 남는 사람을 출력.
 function solution(n, k) {
-  let queue = [];
+  let queue = Array.from({length : n}, (v, index) => index + 1);
   let count = 0;
-
-  for (let i = 1; i < n+1; i++) {
-    queue.push(i);
-  }
 
   while (queue.length !== 1) {
     count++;
@@ -13,10 +9,23 @@ function solution(n, k) {
       queue = queue.slice(1);
       count = 1;
     }
-    queue.push(queue[0]);
-    queue.shift(queue[0]);
+    queue.push(queue.shift());
   }
   return queue[0];
 }
 
 console.log(solution(8, 3)) // 7
+
+function sol2(n, k){
+  let answer;
+  let queue = Array.from({length : n}, (v, i) => i + 1); 
+
+  while (queue.length) {
+      for (let i = 1; i < k; i++) queue.push(queue.shift());
+      queue.shift();
+      if (queue.length === 1) answer = queue.shift();
+  }  
+  return answer;
+}
+
+console.log(sol2(8, 3));
