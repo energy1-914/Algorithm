548c4c926cdd5d6a24d3518fd76a04997ad22bd4
energy1-914 <dlawldnjs5@daum.net>
2022-12-23 23:56:03 +0900

    시간복잡도 줄이기 : 최대 매출_풀이법추가***

--- Chap.1/Q41.js
+++ Chap.1/Q41.js
@@ -15,9 +15,34 @@ function solution(nums, k) {
   return Math.max(...salesSumArray);
 }
 
-console.log(solution([12, 15, 11, 20, 25, 10, 20, 19, 13, 15], 3)) // 56
-console.log(solution([1, 2, 3, 5, 6, 7, 1, 3, 9], 5)) // 26
-console.log(solution([12, 34, 56, 72, 93, 121, 33, 11, 23, 52], 4)) // 342
+// console.log(solution([12, 15, 11, 20, 25, 10, 20, 19, 13, 15], 3)) // 56
+// console.log(solution([1, 2, 3, 5, 6, 7, 1, 3, 9], 5)) // 26
+// console.log(solution([12, 34, 56, 72, 93, 121, 33, 11, 23, 52], 4)) // 342
+
+//*** two pointers 이용한 sliding window ***
+function sol2(nums, k) {
+  let answer;
+  let sum = 0;
+  let lt = 0;
+  
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+  answer = sum;
+
+  for (let rt = k; rt < nums.length; rt++) {
+    sum += (nums[rt] - nums[lt]);
+    answer = Math.max(sum, answer);
+    lt++;
+  }
+  return answer;
+}
+
+console.log(sol2([12, 15, 11, 20, 25, 10, 20, 19, 13, 15], 3)) // 56
+console.log(sol2([1, 2, 3, 5, 6, 7, 1, 3, 9], 5)) // 26
+console.log(sol2([12, 34, 56, 72, 93, 121, 33, 11, 23, 52], 4)) // 342
+
+
 
 // O(kn)이라서 limit. O(n)으로 짜야함
 function solution(nums, k) {
@@ -34,6 +59,6 @@ function solution(nums, k) {
   return Math.max(...salesSumArray);
 }
 
-console.log(solution([12, 15, 11, 20, 25, 10, 20, 19, 13, 15], 3)) // 56
-console.log(solution([1, 2, 3, 5, 6, 7, 1, 3, 9], 5)) // 26
-console.log(solution([12, 34, 56, 72, 93, 121, 33, 11, 23, 52], 4)) // 342
+// console.log(solution([12, 15, 11, 20, 25, 10, 20, 19, 13, 15], 3)) // 56
+// console.log(solution([1, 2, 3, 5, 6, 7, 1, 3, 9], 5)) // 26
+// console.log(solution([12, 34, 56, 72, 93, 121, 33, 11, 23, 52], 4)) // 342
