6f40904f06a13ad17d80a1bef5081b95b8ebcd86
energy1-914 <dlawldnjs5@daum.net>
2022-12-27 17:12:25 +0900

    시간복잡도 줄이기 : 연속된 자연수의 합

--- Chap.1/Q46.js
+++ Chap.1/Q46.js
@@ -2,15 +2,11 @@
 // 매개변수 n이 주어지면 2개 이상의 연속된 자연수의 합이 n이 되는 경우의 수를 반환
 function solution(n) {
   let baseNum = Math.ceil(n/2);
-  let arr = [];
+  let arr = Array.from({length:baseNum}, (v, i) => i+1);
   let sum = 0;
   let lt = 0;
   let answer = 0;
-
-  for (let i = 1; i <= baseNum; i++) {
-    arr.push(i);
-  }
-
+console.log(arr)
   for (let rt = 0; rt < baseNum; rt++) {
     sum += arr[rt];
 
