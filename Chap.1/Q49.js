1f29c9d94f3f5d3b04d99bb389e7315af7479295
energy1-914 <dlawldnjs5@daum.net>
2022-12-29 22:58:41 +0900

    정렬 : 삽입정렬_풀이법 추가

--- Chap.1/Q49.js
+++ Chap.1/Q49.js
@@ -1,4 +1,4 @@
-//삽입정렬
+// 삽입정렬
 function solution(nums) {
 
   for (let i = 0; i < nums.length - 1; i++) {
@@ -12,5 +12,23 @@ function solution(nums) {
   }
   return nums;
 }
-console.log(solution([2, 8, 3, 6, 1, 7, 5, 9])) // [1, 2, 3, 5, 6, 7, 8, 9]
-console.log(solution([7, 6, 10, 9, 8])) // [6, 7, 8, 9, 10]
+// console.log(solution([2, 8, 3, 6, 1, 7, 5, 9])) // [1, 2, 3, 5, 6, 7, 8, 9]
+// console.log(solution([7, 6, 10, 9, 8])) // [6, 7, 8, 9, 10]
+
+function sol2(nums) {
+  let n = nums.length;
+
+  for (let i = 1; i < n; i++) {
+    let tmp = nums[i];
+    let j;
+    for (j = i - 1; j >= 0; j--) {
+      if (nums[j] > tmp) nums[j+1] = nums[j];
+      else break;
+    }
+    nums[j + 1] = tmp;
+  }
+  return nums;
+}
+
+console.log(sol2([2, 8, 3, 6, 1, 7, 5, 9])) // [1, 2, 3, 5, 6, 7, 8, 9]
+console.log(sol2([7, 6, 10, 9, 8])) // [6, 7, 8, 9, 10]
