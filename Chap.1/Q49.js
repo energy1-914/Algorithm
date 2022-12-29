fb0c8074e0b0935feee2db401ec49c539d642d57
energy1-914 <dlawldnjs5@daum.net>
2022-12-29 21:59:46 +0900

    정렬 : 삽입정렬

---
+++ Chap.1/Q49.js
@@ -0,0 +1,16 @@
+//삽입정렬
+function solution(nums) {
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    let key = i + 1;
+    for (let j = i; j >= 0; j--) {
+      if (nums[key] < nums[j]) {
+        [nums[key], nums[j]] = [nums[j], nums[key]];
+        key--;
+      }
+    }
+  }
+  return nums;
+}
+console.log(solution([2, 8, 3, 6, 1, 7, 5, 9])) // [1, 2, 3, 5, 6, 7, 8, 9]
+console.log(solution([7, 6, 10, 9, 8])) // [6, 7, 8, 9, 10]
