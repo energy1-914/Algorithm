56f4dcedc29e65d45c8c540cffc365bf9c83bfa3
energy1-914 <dlawldnjs5@daum.net>
2022-12-30 00:27:27 +0900

    정렬 : 교환 횟수_fix

--- Chap.1/Q50.js
+++ Chap.1/Q50.js
@@ -3,14 +3,14 @@ function solution(nums) {
   let n = nums.length;
   let arr = Array(n).fill(0);
 
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < n - 1; i++) {
     let minIndex = i;
-    for (let j = i; j < n; j++) {
+    for (let j = i + 1; j < n; j++) {
       if (nums[minIndex] > nums[j]) minIndex = j;
     }
-    [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]];
     
     if (i !== minIndex) {
+      [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]];
       arr[i]++;
       arr[minIndex]++;
     }
@@ -21,4 +21,4 @@ function solution(nums) {
 
 console.log(solution([5, 4, 2, 1, 3])) // [1, 1, 2, 2, 2]
 console.log(solution([6, 7, 8, 9, 10])) // [0, 0, 0, 0, 0]
-console.log(solution([2, 1, 4, 3, 0])) // [0, 0, 0, 0, 0]
+console.log(solution([2, 1, 4, 3, 0])) // [1, 0, 1, 0, 2]
