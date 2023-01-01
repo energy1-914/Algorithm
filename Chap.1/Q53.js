dec4f6bf86a921412c0506ffed832375b7f61b85
energy1-914 <dlawldnjs5@daum.net>
2023-01-01 23:19:07 +0900

    그리디 : 침몰하는 타이타닉

---
+++ Chap.1/Q53.js
@@ -0,0 +1,24 @@
+// 침몰하는 타이타닉
+function solution(nums, m) {
+  let [lt, answer] = [0, 0];
+  let rt = nums.length - 1;
+  nums.sort((a,b) => a-b);
+  
+  while (lt <= rt) {
+    if (nums[lt] + nums[rt] <= m) {
+      answer++;
+      lt++;
+      rt--;
+    }
+    else {
+      answer++;
+      rt--;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution([90, 50, 70, 100, 60], 140));
+console.log(solution([86, 95, 107, 67, 38, 49, 116, 22, 78, 53], 150));
+console.log(solution([68, 72, 30, 105, 55, 115, 36, 67, 119, 111, 95, 24, 25, 80, 55, 85, 75, 83, 21, 81], 120));
