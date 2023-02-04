9fbacea4a5c6917520d4e39dff84f05c4c7f0852
energy1-914 <dlawldnjs5@daum.net>
2023-02-05 04:01:11 +0900

    이진트리 DFS: 최대점수 구하기

--- Inflearn/Q74.js
+++ Inflearn/Q74.js
@@ -1,9 +1,22 @@
 //최대점수 구하기
-function solution(m, time, point) {
 
+function solution(m, point, time) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let length = point.length;
+
+  function DFS(L, score, totalTime) {
+    if (totalTime > m) return;
+    if (L === length) {
+      answer = Math.max(answer, score);
+    } else {
+      DFS(L + 1, score + point[L], totalTime + time[L]);
+      DFS(L + 1, score, totalTime);
+    }
+  }
+  DFS(0, 0, 0);
+  return answer;
 }
-console.log(solution(20, ))
 
-let ps=[10, 25, 15, 6, 7];
-            let pt=[5, 12, 8, 3, 4]
-            console.log(solution(20, ps, pt));
+let point = [10, 25, 15, 6, 7];
+let time = [5, 12, 8, 3, 4];
+console.log(solution(20, point, time));
