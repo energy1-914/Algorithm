4d33d2f9ad6bc38c9ac1bbeec0141948d04062f1
energy1-914 <dlawldnjs5@daum.net>
2023-02-07 22:26:24 +0900

    DFS: 팩토리얼 풀이법 추가

--- Inflearn/Q76.js
+++ Inflearn/Q76.js
@@ -1,14 +1,27 @@
-// 팩토리얼
-function solution(n) {
-  let answer = 1;
-  function DFS(L) {
-    if (L === 0) return;
+// // 팩토리얼
+// function solution(n) {
+//   let answer = 1;
+//   function DFS(L) {
+//     if (L === 0) return;
+//     else {
+//       answer = answer * L;
+//       DFS(L - 1);
+//     }
+//   }
+//   DFS(n);
+//   return answer;
+// }
+// console.log(solution(5));
+
+function sol2(n) {
+  let answer;
+  function DFS(n) {
+    if (n === 1) return 1;
     else {
-      answer = answer * L;
-      DFS(L - 1);
+      return n * DFS(n - 1);
     }
   }
-  DFS(n);
+  answer = DFS(n);
   return answer;
 }
-console.log(solution(5));
+console.log(sol2(5));
