5b307a4ae87705da1e992829f1850467a17b81ee
energy1-914 <dlawldnjs5@daum.net>
2023-02-09 23:46:27 +0900

    DFS: 수들의 조합

---
+++ Inflearn/Q80.js
@@ -0,0 +1,23 @@
+// 수들의 조합
+function solution(nums, k, m) {
+  let answer = 0;
+  let tmp = Array(k).fill(0);
+  let length = nums.length;
+
+  function DFS(L, s) {
+    if (L === k) {
+      let sum = tmp.reduce((a, b) => a + b, 0);
+      if (sum % m === 0) answer++;
+    } 
+    else {
+      for (let i = s; i < length; i++) {
+        tmp[L] = nums[i];
+        DFS(L + 1, i + 1);
+      }
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
+
+console.log(solution([2, 4, 5, 8, 12], 3, 6));
