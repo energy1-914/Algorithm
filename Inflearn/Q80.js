e6f0e3defbeca33c44a1acf1342bb8636bcb68fa
energy1-914 <dlawldnjs5@daum.net>
2023-02-09 23:50:19 +0900

    DFS: 수들의 조합 풀이법 추가

--- Inflearn/Q80.js
+++ Inflearn/Q80.js
@@ -21,3 +21,24 @@ function solution(nums, k, m) {
 }
 
 console.log(solution([2, 4, 5, 8, 12], 3, 6));
+
+
+function sol2(nums, k, m) {
+  let answer = 0;
+  let length = nums.length;
+
+  function DFS(L, s, sum) {
+    if (L === k) {
+      if (sum % m === 0) answer++;
+    } 
+    else {
+      for (let i = s; i < length; i++) {
+        DFS(L + 1, i + 1, sum + nums[i]);
+      }
+    }
+  }
+  DFS(0, 0, 0);
+  return answer;
+}
+
+console.log(sol2([2, 4, 5, 8, 12], 3, 6));
