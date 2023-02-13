ed2a4e9f6d9a49a145b1616c79ef9333af7b1c52
energy1-914 <dlawldnjs5@daum.net>
2023-02-13 18:44:08 +0900

    dp : 돌다리 건너기

---
+++ Inflearn/Q88.js
@@ -0,0 +1,14 @@
+// dp : 돌다리 건너기
+function solution(n) {
+  let answer;
+  let dy = Array(n + 1).fill(0);
+  dy[1] = 1;
+  dy[2] = 2;
+  for (let i = 3; i <= n + 1; i++) {
+    dy[i] = dy[i - 2] + dy[i - 1];
+  }
+  answer = dy[n + 1];
+  return answer;
+   
+ }
+console.log(solution(7))
