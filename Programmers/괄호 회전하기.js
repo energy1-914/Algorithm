c9ae762b33e0cf81da98cd4d4a2de08809f37837
energy1-914 <dlawldnjs5@daum.net>
2023-09-27 14:37:44 +0900

    괄호 회전하기

--- Programmers/괄호 회전하기.js
+++ Programmers/괄호 회전하기.js
@@ -9,21 +9,15 @@ function solution(s) {
   function isValid(arr) {
     let stack = [];
     for (let i = 0; i < n; i++) {
-      if (pairs[arr[i]]) {
-        // 여는 괄호인 경우
-        stack.push(arr[i]); // 스택에 담기
-      } else {
-        // 닫는 괄호인 경우
-        if (pairs[stack[stack.length - 1]] === arr[i]) {
-          // 스택의 마지막 값과 쌍을 이룰 경우
-          stack.pop(); // 스택에서 꺼내기
-        } else {
+      if (pairs[arr[i]]) {        // 여는 괄호인 경우
+        stack.push(arr[i]);       // 스택에 담기
+      } else {                    // 닫는 괄호인 경우
+        if (pairs[stack.pop()] !== arr[i]) { // 스택의 마지막 값의 pairs와 다를 경우 바르지 않은 괄호 문자열
           return false;
         }
       }
     }
-    if (stack.length) return false; // 스택이 남아있음 올바르지 않은 문자열
-    return true; // 스택이 비어있으므로 올바른 문자열
+    return stack.length ? false : true;
   }
   // 왼쪽으로 x 칸 만큼 회전시키기
   for (let i = 0; i < n; i++) {
