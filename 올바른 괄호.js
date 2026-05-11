b62e221575d3a128d726b41d4bafbdeff663b20c
Jiwon <108319907+energy1-914@users.noreply.github.com>
2026-05-11 15:18:30 +0900

    프로그래머스 올바른 괄호

---
+++ 올바른 괄호.js
@@ -0,0 +1,15 @@
+function solution(s) {
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      count++;
+    } else {
+      if (count === 0) {
+        return false;
+      }
+      count--;
+    }
+  }
+  return count === 0;
+}
